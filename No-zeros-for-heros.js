5482d73fe31a8f6ff707daec30f381e9bf65daeb
Oyabure Zakari <oyabure205@gmail.com>
2025-01-09 13:10:29 +0100

    solve codewar exercise on no zeros for heros

---
+++ No-zeros-for-heros.js
@@ -0,0 +1,16 @@
+/* 
+  Description;
+  Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+*/
+
+//Solution;
+function noBoringZeros(n) {
+  while (n % 10 === 0 && n !== 0) {
+    n /= 10;
+  }
+  return n;
+}
