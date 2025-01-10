9d7ee86689505940af66969c4d4a5e793b7a06c7
Oyabure Zakari <oyabure205@gmail.com>
2025-01-10 22:20:06 +0100

    solve convert to a boolean exercise on from codewars

---
+++ Convert-a-Boolean-to-a-String.js
@@ -0,0 +1,13 @@
+/* 
+  Description ;
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+//Solution;function areYouPlayingBanjo(name) {
+function booleanToString(b) {
+  //your code here
+  const string = String(b);
+  return string;
+}
